040897f99a8df5e252ca6825204c319959a27a30
Chukwu Emmanuel <eechukwu@gmail.com>
2025-12-06 11:28:15 +0000

    Add health probes for Flask app

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello from Flask on GKE via GitHub Actions 🚀"
+def hello():
+    return "Hello from Flask on GKE 2!"
+
+@app.route("/healthz")
+def healthz():
+    # Simple health endpoint for K8s probes
+    return "ok", 200
 
 if __name__ == "__main__":
-    # Flask will listen on all interfaces, port 8080
     app.run(host="0.0.0.0", port=8080)
